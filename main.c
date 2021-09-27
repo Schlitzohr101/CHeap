2d406b6cb4437ed9e98abbda5b481c2c77578bdf
Schlitzohr <wemx836@gmail.com>
2021-09-26 18:58:39 -0700

    finshed allocate and free added in test cases as well

--- main.c
+++ main.c
@@ -42,18 +42,54 @@ void printBlock(const struct Block *block) ;
  * inputs:
  * size - pointer to block in question on heap
  * outputs:
- * return: pointer to newly allocated block
- * creates a heap within the heap at correct location
+ * return: pointer to newly allocated block data, pointer plus overhead
+ * will allocate space for a given size for the list of space attached to the FREE LIST
 */
 void * myAlloc(int size);
 
 
+void myFree(void * data) ;
+
 
 
 int main() {
-    myInitializeHeap(20);
+    myInitializeHeap(1000);
+    printf("freehead at %p \n",FRHD);
     printf("overhead: %d, and pointer: %d  sizes\n",OVRHDSIZE, PTRSIZE);
     printBlock(FRHD);
+    printf("TEST 1\n");
+    int *a = myAlloc(sizeof(int)); // gimme an int.
+    printf("a is at address %p\n", a); // %p prints the memory address of a pointer in hexadecimal.
+    printBlock(FRHD);
+    myFree(a);
+    printf("after free of a \n");
+    printBlock(FRHD);
+    int *b = myAlloc(sizeof(int));
+    printf("b is at address %p\n", b);
+    //should be at the same location if done right
+    printf("TEST 2\n");
+    myInitializeHeap(1000);
+    printf("freehead at %p \n",FRHD);
+    printBlock(FRHD);
+    a = myAlloc(sizeof(int));
+    b = myAlloc(sizeof(int));
+    printf("a is at address %p and b is at address %p \n",a,b);
+    printf("TEST 3\n");
+    myInitializeHeap(1000);
+    printf("freehead at %p \n",FRHD);
+    printBlock(FRHD);
+    a = myAlloc(sizeof(int));
+    b = myAlloc(sizeof(int));
+    printf("middle int at %p \n",b);
+    int * c = myAlloc(sizeof(int));
+    myFree(b);
+    printf("freed b \n");
+    double * d = myAlloc(sizeof(double) * 2);
+    printf("double array is at address %p\n", d);
+    myFree(d);
+    printf("freed d \n");
+    b = myAlloc(sizeof(int));
+    printf("new int at pos :%p \n",b);
     return 0;
 }
 
@@ -71,13 +107,18 @@ void myInitializeHeap(int size) {
 }
 void printBlock(const struct Block* block) {
     printf("printing block\nblock size:%d and has pointer:",block->blockSize);
-    printf("%s \n", block->next_block == NULL ? "false" : "true");
+    printf("%s ", block->next_block == NULL ? "false\n" : "true");
+    if (block->next_block != NULL) {
+        printf(" at %p \n ",block->next_block);
+    }
 }
 void * myAlloc(int size) {
     // TODO: if size is not a multiple of POINTER_SIZE, you
     // must round it *UP* to the next multiple.
     if (size%PTRSIZE != 0) {
-        size = (int) floor(size/(double)PTRSIZE); //get the largest int back from the division and case back into int for size
+        size = (int) ceil(size/(double)PTRSIZE); //get the largest int back from the division and case back into int for size
+        size = size * PTRSIZE;
+        //printf("size %d \n",size);
     }
 
 
@@ -99,10 +140,15 @@ void * myAlloc(int size) {
     // 3. Perform the "walk through a linked list" logic that you
     // learned in 274.
     while (temp != NULL) { // keep going until we walk off the end of the list.
+        printf("temp block size: %d\n", temp->blockSize);
         if (temp->blockSize >= size) { //block is big enough
-            if (temp->blockSize <= chosen_block->blockSize) {//but is it smaller than the current block
+            if (chosen_block == NULL || temp->blockSize <= chosen_block->blockSize) {//but is it smaller than the current block
                 chosen_block = temp;
-                if (chosen_block->blockSize == size);{break;} //stop loop if exact fit
+                if (chosen_block->blockSize == size) {
+                    printf("size %d \n",size);
+                    printf("chosen size: %d \n",chosen_block->blockSize);
+                    printf("perfect fit\n");
+                    break;} //stop loop if exact fit
             }
         }
         //move temp to next
@@ -116,85 +162,78 @@ void * myAlloc(int size) {
     }
 
     //should split if the size leftover is bigger than overhead size
+    printf("chosen block size: %d\n", chosen_block->blockSize);
     if (chosen_block->blockSize - size > OVRHDSIZE) {
-        struct Block *new_block = (struct Block*)((char*)chosen_block + (OVRHDSIZE + size));
-        new_block->blockSize = chosen_block->blockSize - OVRHDSIZE - size;
-        chosen_block->blockSize = size;
-        //set sizes for both new block and chosen block
-        //new : is different of the current free block minus allocation size and overhead
-        //chosen: allocation size
+
     
         // new_block->next_block = chosen_block->next_block;
         if (chosen_block == FRHD) //case splitting head
         {
+            printf("split chose head \n");
             //block is the head block 
             //we can make a new pointer struct to a size + overhead address and say its holding a difference of the selected block's att blcksz - ovrhd
-            // struct Block *new_block = (struct Block*)((char*)chosen_block + (OVRHDSIZE + size));
-            // new_block->blockSize = chosen_block->blockSize - OVRHDSIZE;
-            // chosen_block->blockSize = size;
-            // new_block->next_block = chosen_block->next_block; //new_block will be head so have it point to other free mem
+            struct Block *new_block = (struct Block*)((char*)chosen_block + (OVRHDSIZE + size));
+            new_block->blockSize = chosen_block->blockSize - OVRHDSIZE - size;
+            chosen_block->blockSize = size;
+            new_block->next_block = chosen_block->next_block; //new_block will be head so have it point to other free mem
             FRHD = new_block;
+            chosen_block->next_block = NULL;
 
             //make new pointer from the chosen_block address plus overhead and size
             //then adjust values for both new blocks
             //set head to the new block
         } else { //case splitting middle block
-            //block found is not the head, so there may not be a pointer to use,but that's okay
-
-            chosen_block->next_block = new_block;
+            printf("split chose mid\n");
+            //block found is not the head, so there is pointer chain to chosen_block 
+            //need to make the new block be the one that is going to be allocated...
+            struct Block *new_block = (struct Block*)((char*)chosen_block->next_block + chosen_block->blockSize - size - OVRHDSIZE);
+            new_block->blockSize = size;
+            new_block->next_block = NULL;
+            chosen_block->blockSize = chosen_block->blockSize - size - OVRHDSIZE;
+            //no need to change where chosen block points to as it is still the 
+            //need to set chosen_block to be the new block
+            chosen_block = new_block;
         }
         
     } else {
-        //can't split due to block not having enough space
+        //can't split due to block not having enough space, so have the full space be occupied aka removed from the list
         if (chosen_block== FRHD) {
+            printf("all chose head\n");
             //block is head block but needs to be removed, normall this requires us to skip
             chosen_block->blockSize = size;
             FRHD = chosen_block->next_block;
-            //this could make FRHD point to nothing but hopefully not else no more space to allocate
+            //this could make FRHD point to nothing but that would mean there is no more space to allocate with
         } else {
-
-        }
+            printf("all chose mid\n");
+            // need to remove a block from the middle of the list and
+            //would be super nice if there was a prev option in the list but here we are, creating a loop to get it
+            struct Block * prev;
+            struct Block * temp = FRHD;
+            while (temp != NULL) {
+                if (temp == chosen_block) {
+                    break;
+                }
+                prev = temp;
+                temp = temp->next_block;
+            }
+            //after getting previous, point the next pointer to chosen's next
+            prev->next_block = chosen_block->next_block;
+        }  
     }
-    // Otherwise, we must decide:
-    // a) Do we split the block? yes/no
-    // b) Is this block the head node? yes/no
+    return (void *)((char*) chosen_block+OVRHDSIZE);
+}
 
-    // The four possible answers to a) and b) will give you four branches
-    // to code.
-
-    // Branch 1: we are not splitting the head node.
-    // if (....) 
-
-    // Branch 2: we are splitting the head node.
-    // else if (....)
-
-    // Branch 3: we are not splitting an interior node.
-    // else if (....)
-
-    // Branch 4: we are splitting an interior node.
-    // else {}
-
-    // To reassign chosen_block's next_block pointer, just give it a new value.
-    // FOR EXAMPLE, to make chosen_block point AROUND the block that follows 
-    // (you don't necessarily actually want to do this, just an example)
-    // we would write
-    // chosen_block->next_block = chosen_block->next_block->next_block
-    // just like you did in 274.
-
-    // If you need to "create" a new block, perhaps to split chosen_block itself,
-    // you will simply create a new struct Block* variable and point it
-    // to an address of where you want the new block object to be.
-    // FOR EXAMPLE, if I wanted a new Block to exist exactly 100 bytes 
-    // after where chosen_block currently is, I would write
-
-    // struct Block *new_block = (struct Block*)((char*)chosen_block->nextBlock + 100);
-
-    // and then initialize its fields:
-    // new_block->size = ...;
-    // new_block->next_block = ...;
-
-    // This might mean that chosen_block is now smaller, and I might have
-    // to change its size field...
-
-    // See the PDF for the explanation for all the casting.
+void myFree(void * data) {
+    printf("doing free \n");
+    struct Block * block = (struct Block*) ((char*)data - OVRHDSIZE);
+    struct Block * prev;
+    struct Block * temp = FRHD;
+    while (temp != NULL) {
+        prev = temp;
+        temp = temp->next_block;
+    } //again no prev so no easy way to find anything so go through list and find last block in free list then set the next pointer to block pointer
+    printf("prev size: %d\n", prev->blockSize);
+    prev->next_block = block;
+    block->next_block = NULL;
+    //add pointer to the end
 }
